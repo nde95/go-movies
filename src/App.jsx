aae353b035dd0c9c5ba5d06966ff1f20220476e7
nde95 <ndennison268@gmail.com>
2023-05-17 13:40:20 -0400

    setting conditionals to routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Outlet, Link } from "react-router-dom";
 import Movies from "./components/Movies/movies.component";
+import { useState } from "react";
 
 
 function App() {
+  const [jwtToken, setJwtToken] = useState("");
+
   return (
     <div className="container">
       <div className="row">
@@ -10,7 +13,11 @@ function App() {
           <h1 className="mt-3">Let's watch a movie</h1>
         </div>
           <div className="col text-end">
-            <Link to="/login"><span className="badge bg-success">Login</span></Link>
+            {/* login conditional */}
+            {jwtToken === ""
+            ?  <Link to="/login"><span className="badge bg-success">Login</span></Link>
+            :  <a href="#!"> <span className="badge bg-danger">Log Out</span> </a>
+            }
         </div>
         <hr className="mb-3"></hr>
       </div>
@@ -22,10 +29,15 @@ function App() {
               <Link to="/" className="list-group-item list-group-item-action">Home</Link>
               <Link to="/movies" className="list-group-item list-group-item-action">Movies</Link>
               <Link to="/genres" className="list-group-item list-group-item-action">Genres</Link>
+              {/* admin routes  */}
+              {jwtToken !== "" && 
+              <>
               <Link to="/admin/movie/0" className="list-group-item list-group-item-action">Add Movies</Link>
-              <Link to="/admin" className="list-group-item list-group-item-action">Manage Catalogue</Link>
+              <Link to="/manage-catalogue" className="list-group-item list-group-item-action">Manage Catalogue</Link>
               <Link to="/graphql" className="list-group-item list-group-item-action">GraphQL</Link>
-            </div>
+              </>
+              }            
+              </div>
           </nav>
         </div>
         <div className="col-md-10">
